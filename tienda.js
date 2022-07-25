765ae1770fc5ad20104cb69d3a6719c6e2c7adb6
lucas cavalieri <lucasgabrielcava@hotmail.com>
2022-07-25 16:11:55 -0300

    se agregan las funciones de interacción con el usuario

--- tienda.js
+++ tienda.js
@@ -1,12 +1,81 @@
-//pedir datos al usuario 
 let nombreCompleto = prompt("Ingresa tu Nombre Completo");
-alert("Usuario ingresado: "+ nombreCompleto);
+alert("Nombre ingresado: "+ nombreCompleto);
 
 let numeroTelefonico = prompt("Ingresa tu numero para ponernos en contacto");
-alert("numero ingresado: "+ numeroTelefonico);
+alert("Numero ingresado: "+ numeroTelefonico);
 
-//mostral que producto puede elegir entre los perfumes
+let difusorEnAerosol = 500;
+let difusorTextil = 350;
+let difusorEnVarilla = 500;
+let opcion = mostrarMenu();
+let dineroIngresado = 0;
 
-//segun lo elegido mostrar que fragancias puede elegir
+    if(opcion!=4)
+    {
+      venderProducto(opcion);
+    }
+    else
+    {
+      alert("GRACIAS");
+    }
 
-//sumar el total de lo elegido
+    function mostrarMenu()
+    {
+      return prompt(`Seleccione una opcion: 
+                    1. TEXTIL (${difusorTextil}ARS)
+                    2. AEROSOL (${difusorEnAerosol}ARS)
+                    3. VARILLA (${difusorEnVarilla}ARS)
+                    4. FIN`);
+      
+    }
+
+    function venderProducto(opcion)
+    {
+      switch (opcion) 
+      {
+        case "1":
+        {
+            pedirDinero(difusorTextil)
+            break;
+        }
+        case "2":
+        {
+            pedirDinero(difusorEnAerosol)
+            break;
+        }
+        case "3":
+        {
+            pedirDinero(difusorEnVarilla)
+            break;
+        }
+        default:
+        {
+            alert("opcion invalida")
+            break;
+        }
+        
+      }
+
+    }
+    function pedirDinero(priceProduct)
+    {
+            while(dineroIngresado<priceProduct)
+            {
+              let dinero = Number(prompt("¿Con cuanto va a pagar?"));
+              
+              if(dinero<priceProduct)
+              {
+                  alert("monto invalido")
+              }
+              else{
+                  dineroIngresado+=dinero;
+              }
+            }
+            
+              let cambio = dineroIngresado - priceProduct;
+              alert("Su cambio es: " + cambio);
+              alert("Gracias, nos pondremos en contacto para la entrega de su producto");
+
+    }
+    
+
