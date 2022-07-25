66eda1847778bcc97b5c651693bb8f9a0e7bb0e5
lucas cavalieri <lucasgabrielcava@hotmail.com>
2022-07-25 13:02:38 -0300

    damos forma al proyecto

---
+++ tienda.js
@@ -0,0 +1,12 @@
+//pedir datos al usuario 
+let nombreCompleto = prompt("Ingresa tu Nombre Completo");
+alert("Usuario ingresado: "+ nombreCompleto);
+
+let numeroTelefonico = prompt("Ingresa tu numero para ponernos en contacto");
+alert("numero ingresado: "+ numeroTelefonico);
+
+//mostral que producto puede elegir entre los perfumes
+
+//segun lo elegido mostrar que fragancias puede elegir
+
+//sumar el total de lo elegido
