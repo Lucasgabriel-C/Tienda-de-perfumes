701f65b43bdcf439409d48074c1e32fbb822c290
lucas cavalieri <lucasgabrielcava@hotmail.com>
2022-08-18 19:47:47 -0300

    hoy hicimos arreglos de java

---
+++ productos.js
@@ -0,0 +1,16 @@
+const aerosoles = [{
+    id: 1,
+    imagen: ("Imagenes/aerosol/aerosol.angel.webp"),
+    nombre: "Angel",
+    precio: 500,
+    envase: "aerosol",
+}
+{
+    id: 2,
+    imagen: ("Imagenes/aerosol/aerosol.apple.webp"),
+    nombre: "Angel",
+    precio: 500,
+    envase: "aerosol",  
+}
+
+]
--- tienda.js
+++ tienda.js
@@ -4,43 +4,23 @@ alert("Nombre ingresado: "+ nombreCompleto);
 let numeroTelefonico = prompt("Ingresa tu numero para ponernos en contacto");
 alert("Numero ingresado: "+ numeroTelefonico);
 
-const carro=[];
+let lista=document.getElementById("varilla");
 
-//cards de varillas
-const bamboo={
-    id:1,
-    imagen:"Imagenes/varilla/varilla.bamboo.jfif",
-    nombre:"BAMBOO",
-    precio: 400
-}
+renderizarProductos();
 
-let tarjetas=document.getElementById("varilla");
-tarjetas.innerHTML=`
-    <div class="card" style="width: 18rem;">
-    <img src=${bamboo.imagen} class="card-img-top" alt="bamboo">
-    <div class="card-body">
-        <h5 class="card-title text-center">${bamboo.nombre}</h5>
-        <p class="card-text text-center">$ ${bamboo.precio}</p>
-        <button id="botonVar" class="btn btn-primary">Comprar</button>
-    </div>
-    </div>
-`;
-
-let botonVarilla=document.getElementById("botonVar");
-botonVarilla.addEventListener("click",interactuar);
-function interactuar(){
-    alert("Agregaste bamboo al carrito");
-}
-botonVarilla.onclick = () => {
-    console.log("Agregaste la BAMBOO al carrito");
-    carro.push(bamboo);
-    console.table(carro);
-}
-botonVarilla.onmouseover = () => {
-    botonVarilla.className="btn btn-outline-success ";
-}
-
-botonVarilla.onmouseout = () => {
-    botonVarilla.className="btn btn-primary";
+function renderizarProductos() {
+    for (const aerosol of aerosoles) {
+        lista.innerHTML+=`<div class="card carta" style="width: 16rem; height: 25rem;">
+        <div class="image-tarjeta">
+            <img src="${aerosoles.imagen}" class="w-100 h-100 " alt="Pitanga">
+        </div>
+        <div class="card-body">
+        <h5 class="card-title text-center">${aerosoles.nombre}</h5>
+        <p class="card-text text-center">$${aerosoles.precio}</p>
+        <a href="#" class="btn btn-primary botone">Comprar</a>
+        </div>
+    </div>`;
+    }
+    
 }
 
